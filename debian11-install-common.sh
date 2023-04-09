9c798a6588b437ab39e5425acb6f36b01210c860
hatmaji-ismu-wicaksono <hatmaji.ismu@gmail.com>
2023-04-09 22:20:53 +0700

    improve install common file

--- debian11-install-common.sh
+++ debian11-install-common.sh
@@ -1,1 +1,18 @@
-sudo apt install -y sudo git curl vim htop build-essential
+# install common software
+sudo apt install -y sudo git curl vim htop build-essential net-tools
+curl https://raw.githubusercontent.com/hatmaji-ismu-wicaksono/tools.debian11/env/production/debian11-install-github-cli.sh | sudo bash
+
+# install development sofware
+# == install apache, composer, php and php extensions
+sudo apt install -y php composer apache2
+curl https://raw.githubusercontent.com/hatmaji-ismu-wicaksono/tools.debian11/env/production/debian11-install-php74-extensions.sh | sudo bash
+
+# == install node js lts version
+curl https://raw.githubusercontent.com/hatmaji-ismu-wicaksono/tools.debian11/env/production/debian11-install-n.sh | sudo bash
+
+# == install docker
+curl https://raw.githubusercontent.com/hatmaji-ismu-wicaksono/tools.debian11/env/production/debian11-install-docker.sh | sudo bash
+
+# == install github cli
+curl https://raw.githubusercontent.com/hatmaji-ismu-wicaksono/tools.debian11/env/production/debian11-install-github-cli.sh | sudo bash
+
